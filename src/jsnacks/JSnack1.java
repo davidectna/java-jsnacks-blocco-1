7236b8344b76744e0c654fff1a738c2417af416e
davidectna@gmail.com <INDIRIZZO_E-MAIL>
2022-06-13 12:01:18 +0200

    jsnack1 second commit

--- src/jsnacks/JSnack1.java
+++ src/jsnacks/JSnack1.java
@@ -6,14 +6,17 @@ public class JSnack1 {
 
 	public static void main(String[] args) {
 		
-		String numberChoice;
+		int sume = 0;
+				
+		Scanner scan = new Scanner (System.in);
 		
 		
 		for (int i=0 ; i < 5; i++) {
-		Scanner scan = new Scanner (System.in);
 		System.out.println("Tell me a number of your choice:");
-		numberChoice = scan.nextLine();
-	  }
+		int numberChoice = scan.nextInt();	 
+		sume += numberChoice ;
+		}
 		
+		System.out.println("The final sume is: " + sume);
    }
 }
