fd807466258b2df2671186224727c9861c73864d
davidectna@gmail.com <INDIRIZZO_E-MAIL>
2022-06-13 11:14:36 +0200

    jsnack1 commit

--- src/jsnacks/JSnack1.java
+++ src/jsnacks/JSnack1.java
@@ -1,10 +1,19 @@
 package jsnacks;
 
+import java.util.Scanner;
+
 public class JSnack1 {
 
 	public static void main(String[] args) {
 		
+		String numberChoice;
 		
-	}
-	
+		
+		for (int i=0 ; i < 5; i++) {
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Tell me a number of your choice:");
+		numberChoice = scan.nextLine();
+	  }
+		
+   }
 }
